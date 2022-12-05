55577c5e04c20627d3cbbd34bd841f55bd10f287
Auditya Maulana Adnan <91017662+aud1tya4dnan0@users.noreply.github.com>
2022-12-05 16:33:36 +0700

    first commit

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <label>Email</label>
+      <input type="text" class="" />
+      <label>Password</label>
+      <input type="password" class="" />
+      
+    </div>
+    <div>
+      <button type="submit" class="" >Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import bootstrap from 'bootstrap'
+
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
---
+++ server/src/app.js
@@ -0,0 +1,6 @@
+import express, { application } from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+
+
+const cors = require(cors())
